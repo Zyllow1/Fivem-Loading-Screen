7a814b3ce92fe3861433ab4d42918dd1751e2c6b
Zyllow <124933975+Zyllow1@users.noreply.github.com>
2024-02-20 15:39:52 +0100

    Add files via upload

---
+++ loading-screen/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+loadscreen 'html/index.html'
+
+files {
+    'html/index.html',
+	'html/assets/logo.png',
+    'html/css/style.css',
+	'html/js/main.js',
+    'html/assets/bgvideo.mp4',
+	'html/assets/music.mp3'
+}
+
+client_script 'client/client.lua'
